97c9900698ce01722a4df18b477ac1b5db8a078c
lpxxn <mi_duo@live.com>
2020-06-20 23:15:33 +0800

    test

--- string_demo/demo1/s1.cpp
+++ string_demo/demo1/s1.cpp
@@ -31,4 +31,48 @@ int main() {
 /*
 https://blog.csdn.net/waveviewer/article/details/81565935?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-2.nonecase&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-2.nonecase
 
+
+Yes, in C++ use the + operator for string concatenation. But this will not work:
+
+char[] + char[] + char[]
+convert one array to std::string and it will:
+
+std::string(char[]) + char[] + char[]
+E.g.:
+
+#include <iostream>
+
+int main()
+{
+    const char a[] = "how ";
+    const char b[] = "are ";
+    const char c[] = "you ";
+
+    std::cout << std::string( a + b + c ) << "\n"; // Error
+    std::cout << std::string(a) + b + c  << "\n"; // Fine
+}
+
+当进行string对象和字符串字面值进行连接时，+操作符的左右至少出现一个是string类型的。
+string s1 = "hello";
+
+string s2 = "world";
+
+ 
+
+string s3 = s1 + ",";//ok
+
+string s4 = "hello" + ",";//error
+
+string s5 = s1 + "," + "world";ok
+
+string s6 = "hello" + "," + s2;error
+
+ 
+
+s4错误的原因是试图直接将两个字符串的字面值进行相加；
+
+s5没错因为前面一个+相当于把前面两个先做temp = s1+",";的操作，temp是字符串型,然后将temp与后面的进行相加;
+
+s6错误的原因参照s4、s5就知道了。
+
 */
