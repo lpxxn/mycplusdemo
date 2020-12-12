77dedaaf596dbee4419c12adc0415898af1e3028
lipeng <lipeng@meican.com>
2020-12-12 17:36:11 +0800

    feat: test

--- http_request/src/main.cpp
+++ http_request/src/main.cpp
@@ -1,10 +1,19 @@
+#include <filesystem>
 #include <iostream>
+#include <string>
 #include <ctime>
 #include <regex>
-#include <CLI/CLI11.hpp>
+
 #include <restclient-cpp/restclient.h>
+#include <yaml-cpp/node/parse.h>
+#include <yaml-cpp/yaml.h>
+#include <CLI/CLI11.hpp>
+
+void parseConfig();
 
 int main(int argc, char **argv) {
+
+
     CLI::App app{"App description"};
     // Define options
     int p = 0;
@@ -15,12 +24,24 @@ int main(int argc, char **argv) {
 
     std::cout << "Hello, World!" << std::endl;
     RestClient::init();
-    RestClient::Response r = RestClient::get("https://baseinfo.meican.com");
+    RestClient::Response r = RestClient::get("https://www.baidu.com");
     std::cout << r.body << std::endl;
     auto a = 1;
     time_t t = time(nullptr);
     tm *local_time = localtime(&t);
     std::cout << "hour: " << local_time->tm_hour << " min: " << local_time->tm_min << std::endl;
 
+    parseConfig();
     return 0;
 }
+
+void parseConfig() {
+    std::__fs::filesystem::path config_path = std::__fs::filesystem::current_path() / "config.yaml";
+    std::cout << config_path << std::endl;
+    YAML::Node config = YAML::LoadFile(config_path);
+
+    if (config["lastLogin"]) {
+        std::cout << "Last logged in: " << config["lastLogin"].as<std::string>() << "\n";
+    }
+}
+
