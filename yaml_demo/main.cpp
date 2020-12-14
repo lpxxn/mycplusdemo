9bdab1fe9b6f2469e26bb2c00d513bdb5e0a26ad
lpxxn <mi_duo@live.com>
2020-12-14 23:04:31 +0800

    feat: test

--- yaml_demo/main.cpp
+++ yaml_demo/main.cpp
@@ -43,13 +43,93 @@ namespace YAML {
     };
 }
 
+struct label {
+    std::string app;
+    std::string image;
+};
+
+namespace YAML {
+    template<>
+    struct convert<label> {
+        static Node encode(const label &rhs) {
+            Node node;
+            node.push_back(rhs.app);
+            node.push_back(rhs.image);
+            return node;
+        }
+
+        static bool decode(const Node &node, label &rhs) {
+            std::cout << node.Type() << std::endl;
+            rhs.app = node["app"].as<std::string>();
+            rhs.image = node["image"].as<std::string>();
+            return true;
+        }
+    };
+}
+
+template<typename T>
+struct myt;
+
+template<>
+struct myt<label> {
+    static bool abc() {
+        return true;
+    }
+};
+
+template<>
+struct myt<int> {
+    static bool def() {
+        return true;
+    }
+};
+template <typename T>
+struct my_as_if;
+
+class MyNode {
+public:
+    template<typename T>
+    T as() const {
+        return my_as_if<T>(*this)();
+    }
+};
+
+
+
+template <typename T>
+struct my_as_if {
+    explicit my_as_if(const MyNode& node_) : node(node_) {}
+    const MyNode& node;
+
+    T operator()() const {
+
+        T t;
+        if (myt<T>::abc())
+            return t;
+        throw "err";
+    }
+};
 int main(int argc, char **argv) {
+    //label myl;
+    myt<label>::abc();
+    myt<int>::def();
+    MyNode mn1;
+    mn1.as<label>();
+
+
     std::string config_path = "/Users/li/Developer/cplus_workspace/mycplusdemo/yaml_demo/config.yaml";
     std::cout << config_path << std::endl;
     YAML::Node config = YAML::LoadFile(config_path);
 
     if (config["api"]) {
-        std::cout << "api: " << config["api"].as<std::string>() << "\n";
+        std::cout << "api: " << config["api"].as<std::string>() << std::endl;
+    }
+    std::cout << "api: " << config["api"].Type() << std::endl;
+    std::cout << "v: " << config["v"].as<int>() << std::endl;
+
+    if (config["label"]) {
+        label l = config["label"].as<label>();
+        std::cout << config["label"].Type() << " app: " << l.app << " image: " << l.image << std::endl;
     }
     if (config["containers"]) {
         YAML::Node cn = config["containers"];
