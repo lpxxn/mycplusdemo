795642265c6d40db5a886e9abbe84e25271d8778
lpxxn <mi_duo@live.com>
2020-12-18 15:32:42 +0800

    feat: log

---
+++ http_request/src/config/config.cpp
@@ -0,0 +1,5 @@
+//
+// Created by li on 2020/12/16.
+//
+
+#include "config.h"
---
+++ http_request/src/config/config.h
@@ -0,0 +1,16 @@
+//
+// Created by li on 2020/12/16.
+//
+
+#ifndef HTTP_REQUEST_CONFIG_H
+#define HTTP_REQUEST_CONFIG_H
+
+
+
+class config {
+
+};
+
+
+
+#endif //HTTP_REQUEST_CONFIG_H
